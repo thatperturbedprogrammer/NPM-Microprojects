10e01266bb7ba9365a62939e340e4c9dd21ca6c2
thatperturbedprogrammer <96286692+thatperturbedprogrammer@users.noreply.github.com>
2025-03-14 13:45:40 +0530

    Delete display-time-dayjs-npm directory

--- display-time-dayjs-npm/lib/getnowbrowser.js
+++
@@ -1,6 +0,0 @@
-import { dayjs } from "https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js";
-
-const nowDate = dayjs().format("YYYY-MM-DD");
-const nowTime = dayjs().format("HH:mm:ss");
-const nowWeekDay = dayjs().format("dddd");
-const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
